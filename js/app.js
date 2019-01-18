2493afd07f5312430e249560a8be8be87e29140c
anniekay825@gmail.com <anniekay825@gmail.com>
2019-01-17 21:13:12 -0500

    added map features

--- js/app.js
+++ js/app.js
@@ -100,7 +100,14 @@ $.ajax({
    console.log("Humidity: " + response.main.humidity);
    console.log("Temperature (F): " + response.main.temp);
  });
-Message Input
 
-
-Message Angie
+// Map Details
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 3, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+};
