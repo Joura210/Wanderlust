57a4c815829aa8f13706cad857ea38a9401d2b86
joura <martinezluisjorge86@gmail.com>
2019-01-22 21:27:22 -0500

    null fix

--- js/app.js
+++ js/app.js
@@ -124,7 +124,12 @@ $("#map-continents").CSSMap({
 "responsive": "auto"
 });
 // END OF THE CSSMap;
-
+var userName = localStorage.getItem("name")
+if(userName !== null){
+  $("#userNameDisplay").text(" " + userName);
+}else{
+  $("#userNameDisplay").text(" " + "Traveler");
+}
 });
 
 // var name = "";
